16a2a7ac8caa2a14b75f6d3da4a00b8e345c557b
sagormahamud <mdshagormahamud028@gmail.com>
2023-03-01 00:12:36 +0600

    vercel hosting

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors=require('cors')
 const port=process.env.PORT|5000;
 require('dotenv').config()
@@ -27,6 +27,8 @@ app.get('/', (req, res) => {
 async function run(){
     try{
         const bannerDataCollection = client.db('shoppingMarket').collection('bannerData')
+        const CartCollection = client.db('shoppingMarket').collection('cartCollection')
+        const feturesCollection = client.db('shoppingMarket').collection('feturesData')
 
 app.get("/bannerProduct", async(req,res) => {
 const query={};
@@ -34,10 +36,34 @@ const bannerData=await bannerDataCollection.find(query).toArray();
 res.send(bannerData)
 })
 
+app.get("/features",async (req,res)=>{
+    const query={}
+    const result=await feturesCollection.find(query).toArray();
+    res.send(result)
+})
 
+app.get("/features/:id",async (req,res)=>{
+    const query=(req.params.id);
+    console.log(query)
+    const id={_id: ObjectId(query)}
+    const result=await feturesCollection.findOne(id);
+    res.send(result)
+})
 
+app.post("/cartData",async(req,res)=>{
+const cartData=req.body;
+const result=await CartCollection.insertOne(cartData);
+res.send(result)
+})
 
-    }
+app.get("/cartData", async(req,res) => {
+    const email=req.query.email;
+    const query={email:email};
+    const cartData=await CartCollection.find(query).toArray();
+    res.send(cartData)
+    })
+
+}
     finally{
 
     }
